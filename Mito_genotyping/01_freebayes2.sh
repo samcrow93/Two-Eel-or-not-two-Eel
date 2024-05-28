1c7fc13012baaf3cc58e52d67a176b07c1c24008
samcrow93 <95499105+samcrow93@users.noreply.github.com>
2024-05-28 10:54:43 -0300

    Update 01_freebayes2.sh

--- Mito_genotyping/01_freebayes2.sh
+++ Mito_genotyping/01_freebayes2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
-#SBATCH --account=def-ibradbur
+#SBATCH --account=
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=samantha.crowley@dal.ca
+#SBATCH --mail-user=
 #SBATCH --mem=191840M
 #SBATCH --cpus-per-task=44
 #SBATCH --nodes=1
